35c4b1a528989dbf75f727957d597a1fd66f12b0
AmalDas <amaldasxda@gmail.com>
2021-11-02 03:27:29 -0400

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -13,25 +13,27 @@ def main( ):
     # paste the IP address of the cluster from step 1
     url = ""
 
-#     driver = webdriver.Remote(
-#         command_executor=url,
-#         desired_capabilities=capabilities
-#     )
+    driver = webdriver.Remote(
+        command_executor=url,
+        desired_capabilities=capabilities
+    )
 
-#     driver.get(url_one)
+    driver.get(url_one)
 
-#     html_page = driver.page_source
+    html_page = driver.page_source
 
-#     print(html_page)
+    print(html_page)
 
-#     print("saving the source page")
+    print("saving the source page")
 
-#     with open("page_source.html", "w") as f:
-#         f.write(html_page)
+    with open("page_source.html", "w") as f:
+        f.write(html_page)
     
 
 
 if __name__ == "__main__":
-#     main( )
+    main( )
     cwd = os.getcwd()
+    arr = os.listdir()
     print(cwd)
+    print(arr)
